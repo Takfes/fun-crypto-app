4bec367e161b93d72379abad34360d9b7abb9ce2
prekss <johnprek@gmail.com>
2021-09-03 21:23:01 +0300

    binance futures db file 1min

---
+++ db_futures1/binance_futures_create.py
@@ -0,0 +1,43 @@
+import os
+import sqlite3
+from sqlite3 import Error
+from sqlite3.dbapi2 import connect
+import config
+
+sql_create_asset_table = """CREATE TABLE IF NOT EXISTS {} (	
+                            symbol TEXT NOT NULL,
+                            openTimets INTEGER NOT NULL,
+                            closeTimets INTEGER NOT NULL,
+                            openTime TEXT NOT NULL,
+                            closeTime TEXT NOT NULL,
+                            open REAL NOT NULL,
+                            high REAL NOT NULL,
+                            low REAL NOT NULL,
+                            close REAL NOT NULL,
+                            volume INTEGER NOT NULL,
+                            numTrades INTEGER NOT NULL,
+                            quoteAssetVolume REAL NOT NULL,
+                            takerBuyBaseAssetVolume INTEGER NOT NULL,
+                            takerBuyQuoteAssetVolume REAL NOT NULL,
+                            UNIQUE(openTimets,symbol)
+                        );""".format(config.DB_ASSET_TABLE_FUTURES_1)
+                        
+sql_create_asset_table_index = """CREATE INDEX {} ON {} (openTimets,symbol);""".format(config.DB_ASSET_TABLE_FUTURES_1_INDEX,config.DB_ASSET_TABLE_FUTURES_15)
+
+if __name__ == '__main__':
+    
+    # find dir path
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    
+    # DB_FILE = dir_path + DB_NAME coming from config
+    # db_file = '/home/takis/Desktop/sckool/my-crypto-app/data/assets.db'
+    db_file = os.path.join(dir_path,config.DB_NAME)
+    print(f'db_file:{db_file}')
+    
+    # connect to db & execute queries
+    con = sqlite3.connect(db_file)
+    cursor = con.cursor()
+    cursor.execute(sql_create_asset_table)
+    cursor.execute(sql_create_asset_table_index)
+    con.commit()
+    con.close()
---
+++ db_futures1/binance_futures_populate.py
@@ -0,0 +1,51 @@
+from tqdm import tqdm
+import os, time
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime
+import config
+
+# define constants
+DB_NAME = config.DB_NAME
+TABLE_NAME = config.DB_ASSET_TABLE_FUTURES_1
+
+if __name__ == '__main__':
+
+    start_ttl = time.time()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    db_file = os.path.join(dir_path, DB_NAME)
+    print(f'db_file:{db_file}')
+
+    pickles = [x for x in os.listdir() if x.endswith('.pkl')]
+
+    con = sqlite3.connect(db_file)
+    cursor = con.cursor()
+
+    for pic in pickles:
+        start = time.time()
+        ttl_rows = 0
+        raw = pd.read_pickle(pic)
+        df = raw.copy()
+
+        df["latest"] = df.groupby(["symbol"])["openTimets"].transform("max")
+        df = df.query("openTimets != latest").drop("latest", axis = 1).assign(
+                openTime = lambda x: x.openTime.astype(str),
+                closeTime = lambda x: x.closeTime.astype(str))
+
+        print(f'File read successfully {pic} with {df.shape[0]} rows')
+
+        for i,row in tqdm(df.iterrows()):
+            try:
+                sql_insert_asset_table = 'INSERT INTO {} {} VALUES {}'.format(TABLE_NAME,tuple(dict(row).keys()),tuple(dict(row).values()))
+                cursor.execute(sql_insert_asset_table)
+                ttl_rows += 1
+            except Exception as e:
+                print(e)
+
+        end = time.time()
+        print(f'File {pic} inserted {ttl_rows} entries in {int(end - start)} seconds')
+        con.commit()
+    con.close()
+    end_ttl = time.time()
+    print(f'Total process lasted {int(end_ttl - start_ttl)} seconds')
