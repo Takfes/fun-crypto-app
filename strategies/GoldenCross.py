1c1988756950a10f2a31e5c494e561a47b8ca8ab
Takfes <takis_a29@hotmail.com>
2021-09-04 20:19:13 +0300

    changes in GoldenCross strategy

--- strategies/GoldenCross.py
+++ strategies/GoldenCross.py
@@ -3,28 +3,59 @@ import backtrader as bt
 
 class GoldenCross(bt.Strategy):
 
-    params = (('fast',50),('slow',200),('order_percentage',0.95),('ticker','ETH'))
+    params = (
+        ('symbol','None'),
+        ('risk',0.1),
+        ('fast',100),
+        ('slow',300)
+        )
         
     def __init__(self):
-        
+
         self.fast_ma = bt.indicators.SMA(
-            self.data.close,period=self.params.fast,plotname=f'{self.params.fast} moving average'
+            self.data.close,period=self.params.fast,plotname=f'{self.p.fast} moving average'
         )
         self.slow_ma = bt.indicators.SMA(
-            self.data.close,period=self.params.slow,plotname=f'{self.params.slow} moving average'
+            self.data.close,period=self.params.slow,plotname=f'{self.p.slow} moving average'
         )
         self.crossover = bt.indicators.CrossOver(self.fast_ma,self.slow_ma)
-                
+
+    # def print_parameters(self):
+    #     print(f'\n====================================\n')
+    #     print(f'This is the GoldenCross print_parameters :')
+    #     for k,v in self.params._getitems():
+    #         print(f'* {k} : {v}')
+    #     print(f'\n====================================\n')
+    
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.datetime(0)
+        print(f'{dt.isoformat()} {txt}')  # Print date and close
+        
+    def sizer(self):
+        amount_to_invest = (self.params.risk * self.broker.cash)
+        size = self.size = round((amount_to_invest / self.datas[0].close), 3)
+        return size
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                # self.log(f'BUY EXECUTED : {order.executed.price}')
+                self.executed_price = order.executed.price
+            elif order.issell():
+                # self.log(f'SELL EXECUTED : {order.executed.price}')
+                self.executed_price = order.executed.price
+            self.bar_executed = len(self)
+        self.order = None
+        
     def next(self):
         
-        if self.position.size == 0:
+        if not self.position:
+            
             if self.crossover > 0 :
-                amount_to_invest = (self.params.order_percentage * self.broker.cash)
-                # self.size = math.floor(amount_to_invest/self.data.close)
-                self.size = round(amount_to_invest/self.data.close,0)
-                # print(f'Buy {self.size} shares of {self.params.ticker} at {self.data.close[0]}')
-                self.buy(size=self.size)
+                self.buy(size=self.sizer())
         
         if self.crossover < 0 :
-            # print(f'Sell {self.size} shares of {self.params.ticker} at {self.data.close[0]}')
             self.close()
+            
